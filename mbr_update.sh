5063440b9763c67e6f74c56e3fef6f0b115bfb32
arnova <arnova@db2cb06f-f8f6-4f7e-9a6e-6049c98cff44>
2012-07-25 12:08:04 +0000

    added: Sanity checking to mbr_update.sh changed: Allow specifying source file/target device for mbr_update.sh

--- mbr_update.sh
+++ mbr_update.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
-# Get target device from 1st argument and remove /dev/
-TARGET_DEVICE=`echo "$1" |sed s,'/dev/',,g`
-
-if [ -z "$TARGET_DEVICE" ]; then
-  echo "Usage: mbr_update.sh {target_device}"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "ERROR: Bad or missing arguments"
+  echo "Usage: mbr_update.sh [source_file] [target_device]" >&2
   exit 1
 fi
 
+SOURCE_FILE="$1"
+TARGET_DEVICE=`echo "$2" |sed s,'/dev/',,g`
+
+if [ ! -f "$SOURCE_FILE" ]; then
+  echo "ERROR: Source file ($SOURCE_FILE) not found!" >&2
+  exit 2
+fi
+
+if [ ! -b "$SOURCE_FILE" ]; then
+  echo "ERROR: Target device ($TARGET_DEVICE) not found!" >&2
+  exit 3
+fi
+
 # Update track0, while preserving current partition table
-dd if=track0.sda of=/dev/$TARGET_DEVICE bs=446 count=1 && dd if=track0.sda of=/dev/$TARGET_DEVICE seek=512 skip=512
+dd if="$SOURCE_FILE" of=/dev/$TARGET_DEVICE bs=446 count=1 && dd if="$SOURCE_FILE" of=/dev/$TARGET_DEVICE seek=512 skip=512
