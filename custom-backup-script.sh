96208d8672bbb9d020465e34e3ee70b416de5b13
arnova <arnova@db2cb06f-f8f6-4f7e-9a6e-6049c98cff44>
2013-09-24 12:03:43 +0000

    fixed: Variable can only be local in a function

--- custom-backup-script.sh
+++ custom-backup-script.sh
@@ -3,7 +3,7 @@
 # Auto remove pagefile.sys & hibernate.sys from NTFS Windows partitions
 IFS=' '
 for PART_NODEV in $BACKUP_PARTITIONS; do
-  local TYPE=`blkid -s TYPE -o value "/dev/${PART_NODEV}"`
+  TYPE=`blkid -s TYPE -o value "/dev/${PART_NODEV}"`
   if [ "$TYPE" = "ntfs" ]; then
     if mkdir -p /mnt/windows && ntfs-3g "/dev/${PART_NODEV}" /mnt/windows; then
       rm -fv /mnt/windows/hiberfile.sys
