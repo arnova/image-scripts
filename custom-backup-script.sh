2162dd7b45606f1900df26ecad2095aa46f0c87f
arnova <arnova@db2cb06f-f8f6-4f7e-9a6e-6049c98cff44>
2013-10-23 06:37:48 +0000

    updated: custom-backup-script.sh

--- custom-backup-script.sh
+++ custom-backup-script.sh
@@ -5,11 +5,13 @@ IFS=' '
 for PART_NODEV in $BACKUP_PARTITIONS; do
   TYPE=`blkid -s TYPE -o value "/dev/${PART_NODEV}"`
   if [ "$TYPE" = "ntfs" ]; then
-    if mkdir -p /mnt/windows && ntfs-3g "/dev/${PART_NODEV}" /mnt/windows; then
-      rm -fv /mnt/windows/hiberfil.sys
-#      rm -fv /mnt/windows/pagefile.sys
+    if mkdir -p /mnt/windows && ntfs-3g "/dev/${PART_NODEV}" /mnt/windows && cd /mnt/windows; then
+      printf "* /dev/$PART_NODEV: "
+      rm -fv hiberfil.sys
+#      rm -fv pagefile.sys
     fi
 
     umount /mnt/windows
   fi
 done
+
