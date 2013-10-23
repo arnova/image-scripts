11ca0b60e71d5d08dd38559777840c6fd3334bf4
arnova <arnova@db2cb06f-f8f6-4f7e-9a6e-6049c98cff44>
2013-10-23 07:11:03 +0000

    changed: Improve custom-backup-script.sh a bit

--- custom-backup-script.sh
+++ custom-backup-script.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# Auto remove pagefile.sys & hibernate.sys from NTFS Windows partitions
+# Auto remove pagefile.sys & hiberfil.sys from NTFS Windows partitions
 IFS=' '
 for PART_NODEV in $BACKUP_PARTITIONS; do
   TYPE=`blkid -s TYPE -o value "/dev/${PART_NODEV}"`
   if [ "$TYPE" = "ntfs" ]; then
-    if mkdir -p /mnt/windows && ntfs-3g "/dev/${PART_NODEV}" /mnt/windows && cd /mnt/windows; then
-      printf "* /dev/$PART_NODEV: "
-      rm -fv hiberfil.sys
-#      rm -fv pagefile.sys
+    if mkdir -p /mnt/temp && ntfs-3g "/dev/${PART_NODEV}" /mnt/temp; then
+      if [ -f "/mnt/temp/hiberfil.sys" ]; then
+        echo "* Remove /dev/$PART_NODEV/hiberfil.sys"
+        rm /mnt/temp/hiberfil.sys
+      fi
+      if [ -f "/mnt/temp/pagefile.sys" ]; then
+        echo "* Remove /dev/$PART_NODEV/pagefile.sys"
+        rm /mnt/temp/pagefile.sys
+      fi
     fi
-
+    
     umount /mnt/windows
   fi
 done
