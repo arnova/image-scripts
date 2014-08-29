1e1095c7425809f476ec20776d1966c2f28dbea7
arnova <arnova@db2cb06f-f8f6-4f7e-9a6e-6049c98cff44>
2014-08-29 09:54:03 +0000

    changed: Bump version to 3.10 (stable) for image scripts

--- restore-image.sh
+++ restore-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MY_VERSION="3.10-BETA26"
+MY_VERSION="3.10"
 # ----------------------------------------------------------------------------------------------------------------------
 # Image Restore Script with (SMB) network support
-# Last update: May 19, 2014
+# Last update: August 29, 2014
 # (C) Copyright 2004-2014 by Arno van Amersfoort
 # Homepage              : http://rocky.eld.leidenuniv.nl/
 # Email                 : a r n o v a AT r o c k y DOT e l d DOT l e i d e n u n i v DOT n l
