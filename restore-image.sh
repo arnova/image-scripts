6a53aaba544bf2337d8442caa1b1a1e8f815c886
arnova <arnova@void.org>
2015-07-16 08:47:33 +0200

    changed: Simulate partition table restore to make sure disk is eg. large enough

--- restore-image.sh
+++ restore-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-MY_VERSION="3.11h"
+MY_VERSION="3.11i"
 # ----------------------------------------------------------------------------------------------------------------------
 # Image Restore Script with (SMB) network support
-# Last update: July 3, 2015
+# Last update: July 16, 2015
 # (C) Copyright 2004-2015 by Arno van Amersfoort
 # Homepage              : http://rocky.eld.leidenuniv.nl/
 # Email                 : a r n o v a AT r o c k y DOT e l d DOT l e i d e n u n i v DOT n l
@@ -1278,6 +1278,29 @@ check_disks()
       echo "" >&2
       printf "Press <enter> to continue or CTRL-C to abort...\n" >&2
       read dummy
+    fi
+
+    # Simulate DOS partition table restore
+    if [ -e "sfdisk.${IMAGE_SOURCE_NODEV}" ]; then
+      result="$(cat "sfdisk.${IMAGE_SOURCE_NODEV}" |sfdisk -n 2>&1 >/dev/null)"
+
+      # For now only check for max size errors
+      if [ $? -ne 0 ] && echo "$result" |grep -q -e 'exceeds max allowable size'; then
+        echo "$result" >&2
+        printf "\033[40m\033[1;31m\nERROR: Invalid DOS partition table (disk too small?)! Quitting...\n\033[0m" >&2
+        do_exit 5
+      fi
+    fi
+
+    # Simulate GPT partition table restore
+    if [ -e "sgdisk.${IMAGE_SOURCE_NODEV}" ]; then
+      result="$(sgdisk --pretend --load-backup="sgdisk.${IMAGE_SOURCE_NODEV}" 2>&1 >/dev/null)"
+
+      if [ $? -ne 0 ]; then
+        echo "$result" >&2
+        printf "\033[40m\033[1;31m\nERROR: Invalid GPT partition table (disk too small?)! Quitting...\n\033[0m" >&2
+        do_exit 5
+      fi
     fi
 
     DEVICE_FILES="${DEVICE_FILES}${DEVICE_FILES:+ }${IMAGE_SOURCE_NODEV}${SEP}${TARGET_NODEV}"
