57af03650df522d88654fe161db7560a4efd2d7c
arnova <arnova@void.org>
2015-12-09 15:58:27 +0100

    changed: Cleanup + improve mkuserdisk.sh script

--- mkuserdisk.sh
+++ mkuserdisk.sh
@@ -3,32 +3,35 @@
 ####################
 # Global variables #
 ####################
+# Globals sourced from main script: CLEAN, TARGET_NODEV, TARGET_DEVICES
 
 # The primary partition number to use for the user partition (with Win7, 1 and 2 are used by the OS)
 USER_PART_ID="3"
 
-USER_DISK=""
 USER_DISK_NODEV=""
-USER_DISK_ON_OTHER_DEVICE=0
-EMPTY_PARTITION_TABLE=0
+EXTRA_DISKS_NODEV=""
 
 #############
 # Functions #
 #############
 
+# Arguments: $1 = DISK, $2 = Partition ID, $3 = Start with empty partition table(1)
 mkud_create_user_dos_partition()
 {
-  local PART_ID="$USER_PART_ID"
+  local DOS_DISK="$1"
+  local DOS_PART_ID="$2"
+  local DOS_PART="${DOS_DISK}${DOS_PART_ID}"
+  local EMPTY_PARTITION_TABLE="$3"
   local FDISK_CMD=""
 
-  # TODO?: Use parted -s and auto detect PART_ID
+  # TODO?: Use parted -s and auto detect USER_PART_ID
   if [ $EMPTY_PARTITION_TABLE -eq 1 ]; then
     echo "* Creating new (empty) DOS partition"
     # Empty partition table"
     FDISK_CMD="o
 n
 p
-$PART_ID
+$DOS_PART_ID
 
 
 t
@@ -39,29 +42,33 @@ w
     echo "* Adding user partition to (existing) DOS partition"
     FDISK_CMD="n
 p
-$PART_ID
+$DOS_PART_ID
 
 
 t
-$PART_ID
+$DOS_PART_ID
 7
 w
 "
   fi
 
   # Create NTFS partition:
-  echo "* Creating user NTFS DOS partition $USER_PART"
-  if ! echo "$FDISK_CMD" |fdisk $USER_DISK >/dev/null; then
+  echo "* Creating user NTFS DOS partition $DOS_PART"
+  if ! echo "$FDISK_CMD" |fdisk $DOS_DISK >/dev/null; then
     return 1
   fi
-  
+
   return 0
 }
 
 
+# Arguments: $1 = DISK, $2 = Partition ID
 mkud_create_user_gpt_partition()
 {
-  local PART_ID="$USER_PART_ID"
+  local GPT_DISK="$1"
+  local GPT_PART_ID="$2"
+  local GPT_PART="${GPT_DISK}${GPT_PART_ID}"
+  local EMPTY_PARTITION_TABLE="$3"
   local GDISK_CMD=""
 
   # TODO?: Use parted -s and auto detect PART_ID
@@ -71,7 +78,7 @@ mkud_create_user_gpt_partition()
     GDISK_CMD="o
 y
 n
-$PART_ID
+$GPT_PART_ID
 
 
 0700
@@ -81,7 +88,7 @@ y
   else
     echo "* Adding user partition to (existing) GPT partition"
     GDISK_CMD="n
-$PART_ID
+$GPT_PART_ID
 
 
 0700
@@ -91,9 +98,9 @@ y
   fi
 
   # Create NTFS partition:
-  echo "* Creating user NTFS GPT partition $USER_PART"
+  echo "* Creating user NTFS GPT partition $GPT_PART"
 
-  if ! echo "$GDISK_CMD" |gdisk $USER_DISK >/dev/null; then
+  if ! echo "$GDISK_CMD" |gdisk $GPT_DISK >/dev/null; then
     return 1
   fi
 
@@ -101,53 +108,40 @@ y
 }
 
 
+# Arguemtns: $1 = DISK_NODEV, $2 = partition id, $3 = fileystem label, $4 = (wipe + ) repartition disk(1) or not (0)
 mkud_create_user_filesystem()
 {
-  local PART_ID=$USER_PART_ID
-  local USER_PART="${USER_DISK}${PART_ID}"
-
-  local PARTITIONS_FOUND="$(get_partitions /dev/$USER_DISK_NODEV)"
-
-  if [ $USER_DISK_ON_OTHER_DEVICE -eq 1 ]; then
-    if [ -n "$PARTITIONS_FOUND" ]; then
-      printf "\033[40m\033[1;31m* WARNING: There are already partitions on the seperate disk!\n\033[0m" >&2
-      if ! get_user_yn "* Wipe (repartition + format) and continue"; then
-        return
-      fi
-    fi
-
-    echo "* NOTE: User partition $USER_PART will be created on a seperate disk"
-  fi
+  local TARGET_DISK_NODEV="$1"
+  local TARGET_PART_ID="$2"
+  local TARGET_PART_LABEL="$3"
+  local REPARTITION_DISK="$4"
+  local TARGET_DISK="/dev/${TARGET_DISK_NODEV}"
+  local TARGET_PART="${TARGET_DISK}${TARGET_PART_ID}"
 
   # TODO: Skip the block below if our target disk already *exactly* matches?!
-
-  if ! echo "$PARTITIONS_FOUND" |grep -q "${USER_DISK_NODEV}${PART_ID}$" || [ $CLEAN -eq 1 ]; then
-    # Automatically handle cases where we have 2 harddisks: one for the OS (Eg. ssd) and one for user data and empty disks
-    if [ "$CLEAN" = "1" -a $USER_DISK_ON_OTHER_DEVICE -eq 1 ] || [ -z "$PARTITIONS_FOUND" ]; then
-      EMPTY_PARTITION_TABLE=1
-    fi
-
+  local PARTITIONS_FOUND="$(get_partitions $TARGET_DISK)"
+  if ! echo "$PARTITIONS_FOUND" |grep -q "${TARGET_DISK_NODEV}${TARGET_PART_ID}$" || [ $REPARTITION_DISK -eq 1 ]; then
     # If user partition is on the same device as the images, check that it does not exist already
-    if [ ! -e $USER_PART -o $EMPTY_PARTITION_TABLE -eq 1 ]; then
+    if [ ! -e $TARGET_PART -o $REPARTITION_DISK -eq 1 ]; then
       # Detect GPT. Always use GPT on an empty disk since size may be > 2TB
-      if [ $EMPTY_PARTITION_TABLE -eq 1 ] || sfdisk -d $USER_DISK 2>/dev/null |grep -q -E -i '[[:blank:]]Id=ee'; then
-        mkud_create_user_gpt_partition;
+      if [ $REPARTITION_DISK -eq 1 ] || sfdisk -d $TARGET_DISK 2>/dev/null |grep -q -E -i '[[:blank:]]Id=ee'; then
+        mkud_create_user_gpt_partition $TARGET_DISK $TARGET_PART_ID $REPARTITION_DISK
       else
-        mkud_create_user_dos_partition;
+        mkud_create_user_dos_partition $TARGET_DISK $TARGET_PART_ID $REPARTITION_DISK
       fi
     else
-      echo "* Skipping creation of NTFS partition $USER_PART since it already exists"
+      echo "* Skipping creation of NTFS partition $TARGET_PART since it already exists"
     fi
 
-    if ! partprobe $USER_DISK; then
+    if ! partprobe $TARGET_DISK; then
       printf "\033[40m\033[1;31mWARNING: (Re)reading the partition table failed!\nPress enter to continue or CTRL-C to abort...\n\033[0m" >&2
       read
       echo ""
     fi
 
-    echo "* Creating user NTFS filesystem on $USER_PART"
-    if mkntfs -L USER -Q "$USER_PART"; then
-      if mkdir -p /mnt/windows && ntfs-3g "$USER_PART" /mnt/windows; then
+    echo "* Creating user NTFS filesystem on $TARGET_PART with label \"$TARGET_PART_LABEL\""
+    if mkntfs -L $TARGET_PART_LABEL -Q $TARGET_PART; then
+      if mkdir -p /mnt/windows && ntfs-3g "$TARGET_PART" /mnt/windows; then
         mkdir "/mnt/windows/My Documents"
         mkdir "/mnt/windows/Downloads"
         mkdir "/mnt/windows/temp"
@@ -155,50 +149,44 @@ mkud_create_user_filesystem()
 
         umount /mnt/windows
       else
-        printf "\033[40m\033[1;31mWARNING: Mounting NTFS partition $USER_PART failed (disk in use?!)\n\033[0m" >&2
+        printf "\033[40m\033[1;31mWARNING: Mounting NTFS partition $TARGET_PART failed (disk in use?!)\n\033[0m" >&2
       fi
     else
-      printf "\033[40m\033[1;31mERROR: Creating NTFS filesystem on $USER_PART failed!\033[0m\n" >&2
+      printf "\033[40m\033[1;31mERROR: Creating NTFS filesystem on $TARGET_PART failed!\033[0m\n" >&2
     fi
   else
-    echo "* Skipping creation of NTFS filesystem on $USER_PART since it already exists"
+    echo "* Skipping creation of NTFS filesystem on $TARGET_PART since it already exists"
   fi
 }
 
 
 mkud_select_disk()
 {
-  # Check which partitions we can restore on, we ignore mounted ones
+  # Check which partitions we can use for the user partiton, we ignore mounted ones
   local FIND_DISKS=""
   unset IFS
   for DISK in `cat /proc/partitions |grep -E '[sh]d[a-z]$' |awk '{ print $4 }' |sed s,'^/dev/',,`; do
     # Ignore disks with swap/mounted partitions
     if grep -E -q "^/dev/${DISK}p?[0-9]+" /etc/mtab; then
-      echo "* NOTE: Ignoring mounted disk /dev/$DISK" >&2
+      echo "* NOTE: Ignoring disk with mounted partitions /dev/$DISK" >&2
     elif ! grep -E -q "^/dev/${PART}p?[0-9]+" /proc/swaps; then
       FIND_DISKS="${FIND_DISKS}${FIND_DISKS:+ }$DISK"
     fi
   done
 
   # With more than one disk assume user partition will be on another disk than the OS
-  if [ $(echo "$FIND_DISKS" |wc -w) -gt 1 ]; then
-    IFS=' '
-    for DISK in $FIND_DISKS; do
-      if [ "$DISK" != "$TARGET_NODEV" ] && ! echo "$TARGET_DEVICES" |grep -q -e " ${DISK} " -e "^${DISK} " -e "^${DISK}$" -e " ${DISK}$"; then
-        USER_DISK_NODEV="$DISK"
-        USER_DISK_ON_OTHER_DEVICE=1
-        break;
-      fi
-    done
+  IFS=' '
+  for DISK in $FIND_DISKS; do
+    if [ "$DISK" = "$TARGET_NODEV" ]; then
+      USER_DISK_NODEV="$DISK"
+    elif ! echo "$TARGET_DEVICES" |grep -q -E "(^| )$DISK( |$)"; then
+      EXTRA_DISKS_NODEV="${EXTRA_DISKS_NODEV}${EXTRA_DISKS_NODEV:+ }$DISK"
+    fi
+  done
 
 #    TODO: User selection
 #    echo "Multiple disks found:"
 #    echo "$FIND_DISKS"
-  else
-    # Only one disk, use that as default
-    USER_DISK_NODEV="$FIND_DISKS"
-    USER_DISK_ON_OTHER_DEVICE=0
-  fi
 }
 
 
@@ -212,11 +200,43 @@ mkud_select_disk;
 if [ -z "$USER_DISK_NODEV" ]; then
   echo "WARNING: No (suitable) disk found for user partition!" >&2
 else
-  USER_DISK="/dev/${USER_DISK_NODEV}"
-  mkud_create_user_filesystem;
+  # In principle we should never have to wipe + repartition(0) the OS disk
+  # as this should already be done while restoring the images
+  mkud_create_user_filesystem "$USER_DISK_NODEV" "$USER_PART_ID" "USER" 0
 
   # Add the disk to restore-image script's target list so its partitions get listed when done
-  if ! echo "$TARGET_DEVICES" |grep -q -e " ${USER_DISK} " -e "^${USER_DISK} " -e "^${USER_DISK}$" -e " ${USER_DISK}$"; then
-    TARGET_DEVICES="$TARGET_DEVICES $USER_DISK"
+  if ! echo "$TARGET_DEVICES" |grep -q -E "(^| )/dev/$USER_DISK_NODEV( |$)"; then
+    TARGET_DEVICES="$TARGET_DEVICES /dev/$USER_DISK_NODEV"
   fi
+
+  # Check for extra disk:
+  USER_COUNT=1
+  IFS=' ,'
+  for EXTRA_DISK_NODEV in $EXTRA_DISKS_NODEV; do
+    PARTITIONS_FOUND="$(get_partitions /dev/$EXTRA_DISK_NODEV)"
+    EXTRA_DISK_WIPE=0
+    if [ -n "$PARTITIONS_FOUND" ]; then
+      if [ $CLEAN -eq 1 ]; then
+        printf "\033[40m\033[1;31m* WARNING: Extra disk /dev/$EXTRA_DISK_NODEV already contains partitions!\n\033[0m" >&2
+        if get_user_yn "  Wipe (repartition + format) and format as (additional) user disk"; then
+          EXTRA_DISK_WIPE=1
+        fi
+      else
+        echo "* NOTE: Extra disk /dev/$EXTRA_DISK_NODEV already contains partitions, ignoring it"
+      fi
+    else
+      EXTRA_DISK_WIPE=1
+    fi
+
+    USER_COUNT=$((USER_COUNT + 1))
+
+    if [ $EXTRA_DISK_WIPE -eq 1 ]; then
+      mkud_create_user_filesystem "$EXTRA_DISK_NODEV" "$USER_PART_ID" "USER${USER_COUNT}" 1;
+      # Add the disk to restore-image script's target list so its partitions get listed when done
+      if ! echo "$TARGET_DEVICES" |grep -q -E "(^| )/dev/$EXTRA_DISK_NODEV( |$)"; then
+        TARGET_DEVICES="$TARGET_DEVICES /dev/$EXTRA_DISK_NODEV"
+      fi
+    fi
+  done
 fi
+
