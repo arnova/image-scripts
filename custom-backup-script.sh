60c5f9f1a67d4b2b33085ccdc5cb0acd056c2e69
arnova <arnova@void.org>
2016-08-05 07:21:23 +0200

    fixed: Windows 10 NTFS partitions require -o remove_hiberfile for mounting (bug?) changed: Also remove Windows 10 pagefile.sys from Windows 10 NTFS partitions

--- custom-backup-script.sh
+++ custom-backup-script.sh
@@ -4,11 +4,12 @@ if [ "$NO_IMAGE" != "1" ]; then
   for PART_NODEV in $BACKUP_PARTITIONS; do
     TYPE=`blkid -s TYPE -o value "/dev/${PART_NODEV}"`
     if [ "$TYPE" = "ntfs" ]; then
-      if mkdir -p /mnt/temp && ntfs-3g "/dev/${PART_NODEV}" /mnt/temp; then
-        if [ -f "/mnt/temp/hiberfil.sys" ]; then
-          echo "* Remove /dev/$PART_NODEV/hiberfil.sys"
-          rm /mnt/temp/hiberfil.sys
+      if mkdir -p /mnt/temp && ntfs-3g -o remove_hiberfile "/dev/${PART_NODEV}" /mnt/temp; then
+        if [ -f "/mnt/temp/swapfile.sys" ]; then
+          echo "* Remove /dev/$PART_NODEV/swapfile.sys"
+          rm /mnt/temp/swapfile.sys
         fi
+
         if [ -f "/mnt/temp/pagefile.sys" ]; then
           echo "* Remove /dev/$PART_NODEV/pagefile.sys"
           rm /mnt/temp/pagefile.sys
@@ -19,3 +20,4 @@ if [ "$NO_IMAGE" != "1" ]; then
     fi
   done
 fi
+
